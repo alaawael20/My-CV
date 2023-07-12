b80c9206f32359c8f9d9a09d02f53286681dc205
Alaa El-faqawi <93674732+alaaelfaqawi@users.noreply.github.com>
2023-07-12 16:57:06 +0300

    edit in image source

--- src/components/LeftSide/index.jsx
+++ src/components/LeftSide/index.jsx
@@ -5,11 +5,12 @@ import Name from '../Name'
 import PersonalInformation from '../PersonalInformation'
 import Skills from '../Skills'
 import Contact from '../Contact'
+import profileImg from '../assets/profileImg.jpg'
 
 const LeftSide = () => {
   return (
     <div className='leftSide'>
-      <ProfileImg profileImg= '/assets/profileImg.jpg'/>
+      <ProfileImg profileImg= {profileImg}/>
       <Name FullName='Alaa Wael El-faqawi' SubName='Web Developer'/>
       <PersonalInformation Name='Alaa Wael El-faqawi' Birthdate='24/4/2001' Gender='Male' Nationality='Palestinian'/>
       <Skills />
